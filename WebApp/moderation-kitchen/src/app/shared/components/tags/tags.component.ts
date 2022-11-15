57782bce51018bd324dc3a62fb17a9de8c4291eb
isaac-brown <isaac.joseph.brown@gmail.com>
2022-11-15 22:23:54 +1300

    chore: bind data to tags

--- WebApp/moderation-kitchen/src/app/shared/components/tags/tags.component.ts
+++ WebApp/moderation-kitchen/src/app/shared/components/tags/tags.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tags',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tags.component.scss']
 })
 export class TagsComponent implements OnInit {
+
+  @Input()
+  public tags?: string[];
 
   constructor() { }
 
