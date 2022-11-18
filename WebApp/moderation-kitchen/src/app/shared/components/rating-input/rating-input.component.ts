cb6c3e11a9e9251692c5795794bc30efe8cfef1c
isaac-brown <isaac.joseph.brown@gmail.com>
2022-11-18 16:54:02 +1300

    chore: make rating input dynamic

--- WebApp/moderation-kitchen/src/app/shared/components/rating-input/rating-input.component.ts
+++ WebApp/moderation-kitchen/src/app/shared/components/rating-input/rating-input.component.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar, IconDefinition } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-rating-input',
   templateUrl: './rating-input.component.html',
-  styleUrls: ['./rating-input.component.scss']
+  styleUrls: ['./rating-input.component.scss'],
 })
-export class RatingInputComponent implements OnInit {
+export class RatingInputComponent {
   faStar = faStar;
+  faStarSolid = faStarSolid;
+  public isHovered: boolean = false;
 
-  constructor() { }
+  public icon(star: number) : IconDefinition {
+    if (this.isHovered) {
+      return (star <= this.hoveredUserRating) ? faStarSolid : faStar;
+    }
+    return (star <= this.selectedUserRating) ? faStarSolid : faStar;
+  }
+  
 
-  ngOnInit(): void {
+  public stars: number[] = [1, 2, 3, 4, 5];
+
+  public hoveredUserRating: number = 0;
+  public selectedUserRating: number = 0;
+
+  countStar(star: number) {
+    if (star === this.selectedUserRating) {
+      this.selectedUserRating = 0;
+      return;
+    }
+
+    this.selectedUserRating = star;
   }
 
+  hoverStar(star: number) {
+    this.isHovered = star !== 0;
+    this.hoveredUserRating = star;
+  }
 }
