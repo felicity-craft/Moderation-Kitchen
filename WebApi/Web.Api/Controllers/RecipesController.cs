a3dddc6668480830a40f4fde74f4acb28c267be5
Felicity <felicitycraft22@gmail.com>
2022-11-26 15:09:06 +1300

    feat: implement GET /api/recipes

--- WebApi/Web.Api/Controllers/RecipesController.cs
+++ WebApi/Web.Api/Controllers/RecipesController.cs
@@ -12,15 +12,18 @@ public class RecipesController: ControllerBase
     private readonly string dataDirectoryPath = "/Users/fliss/Desktop/VS Projects/ModerationKitchen/WebApi/Web.Api/Data";
     private readonly IFileSystem fileSystem;
     private readonly JsonSerializerOptions jsonOptions;
+    private readonly ILogger<RecipesController> logger;
 
-    public RecipesController(IFileSystem fileSystem, JsonSerializerOptions jsonOptions)
+    public RecipesController(IFileSystem fileSystem, JsonSerializerOptions jsonOptions, ILogger<RecipesController> logger)
     {
         this.fileSystem = fileSystem;
         this.jsonOptions = jsonOptions;
+        this.logger = logger;
     }
 
     [HttpGet("{slug}")]
     public async Task<IActionResult> GetBySlug([FromRoute] string slug, CancellationToken ct){
+        this.logger.LogInformation("Trying to get recipe with slug {slug}", slug);
         string recipeFilePath = Path.Join(this.dataDirectoryPath, $"{slug}.json");
         if (this.fileSystem.File.Exists(recipeFilePath))
         {
@@ -31,7 +34,27 @@ public class RecipesController: ControllerBase
             var recipe = await JsonSerializer.DeserializeAsync<Recipe>(stream, this.jsonOptions, ct);
             return this.Ok(recipe);
         }
+        this.logger.LogWarning("No recipe found with slug {slug}", slug);
         return this.NotFound();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllRecipes(CancellationToken ct){
+        if (this.fileSystem.Directory.Exists(this.dataDirectoryPath))
+        {
+            // Loops through each json file in our dataDirectory and converts the contents of each file into a Recipe object.
+            // Each Recipe object is added to a single list called recipes which gets returned.
+            string[] jsonFilePaths = this.fileSystem.Directory.GetFiles(this.dataDirectoryPath, "*.json");
+            var recipes = new List<Recipe>();
+            foreach (var filePath in jsonFilePaths)
+            {
+                using Stream stream = this.fileSystem.File.OpenRead(filePath);
+                var recipe = await JsonSerializer.DeserializeAsync<Recipe>(stream, this.jsonOptions, ct);
+                recipes.Add(recipe!);
+            }
+            return this.Ok(recipes);
+        }
+        return this.Ok(Array.Empty<Recipe>());
     }
 
 }
