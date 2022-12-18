beee0ad8fafbbad25ad56f9f9a45e3dc3ab1bab2
Felicity <felicitycraft22@gmail.com>
2022-12-18 14:24:53 +1300

    chore: search container implemented

--- WebApp/moderation-kitchen/src/app/shared/components/search-container/search-container.component.ts
+++ WebApp/moderation-kitchen/src/app/shared/components/search-container/search-container.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { mergeMap, Observable } from 'rxjs';
+import { RecipeService } from 'src/app/core/services/recipe.service';
+
 
 @Component({
   selector: 'app-search-container',
@@ -6,16 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-container.component.scss']
 })
 export class SearchContainerComponent implements OnInit {
-  categories: string[] = [
-    'Cookies',
-    'Cakes',
-    'Chocoalte',
-    'Easy',
-    'Slices',
-  ]
-  constructor() { }
+
+  recipeSearchControl= new FormControl('');
+  filteredRecipes: Observable<{title: string, slug: string}[]>;
+
+  @Input()
+  tags: string[] = []
+
+  constructor(
+    private recipeService: RecipeService,
+    private snackBar: MatSnackBar
+
+  ) { }
 
   ngOnInit(): void {
+    this.filteredRecipes = this.recipeSearchControl.valueChanges.pipe(
+      mergeMap(value => this.recipeService.searchForRecipe(value))
+    );
+  }
+
+  onTagSelected() {
+    this.snackBar.open('Searching by tag is not implemented yet', 'Close')
   }
 
 }
