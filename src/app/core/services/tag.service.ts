55229c6667799cfc27f3c6f0b16f23273d8ba5a8
Felicity <felicitycraft22@gmail.com>
2022-12-22 20:08:59 +1300

    chore: update baseURL

--- src/app/core/services/tag.service.ts
+++ src/app/core/services/tag.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
-  private baseUrl: string = 'https://localhost:7264'
+  private baseUrl: string = environment.apiBaseUrl;
 
   constructor(
     private httpClient: HttpClient
